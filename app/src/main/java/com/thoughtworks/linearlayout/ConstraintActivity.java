9b81aaf9471372a27e237356e913164a5265853e
unknown <hzh_innovation@163.com>
2020-08-11 18:17:44 +0800

    [hzh] feat: add goback of ConstraintActivity

--- app/src/main/java/com/thoughtworks/linearlayout/ConstraintActivity.java
+++ app/src/main/java/com/thoughtworks/linearlayout/ConstraintActivity.java
@@ -1,14 +1,28 @@
 package com.thoughtworks.linearlayout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 public class ConstraintActivity extends Activity {
+    private View.OnClickListener jumpToMainActivity = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(ConstraintActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_activity);
+
+        Button goBack = (Button) findViewById(R.id.goBackToMainFromConstraint);
+        goBack.setOnClickListener(jumpToMainActivity);
     }
 }
