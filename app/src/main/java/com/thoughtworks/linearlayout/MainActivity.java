654ee915e6f96ade81d2b7e867ba22266e606446
unknown <hzh_innovation@163.com>
2020-08-11 12:21:20 +0800

    [hzh] feat: add jump to FrameLayoutActivity

--- app/src/main/java/com/thoughtworks/linearlayout/MainActivity.java
+++ app/src/main/java/com/thoughtworks/linearlayout/MainActivity.java
@@ -1,14 +1,35 @@
 package com.thoughtworks.linearlayout;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button frameButton;
+
+    private View.OnClickListener jump = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btnFrameLayout:
+                    Intent intent = new Intent(MainActivity.this, FrameLayoutActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameButton = (Button) findViewById(R.id.btnFrameLayout);
+        frameButton.setOnClickListener(jump);
     }
 }
